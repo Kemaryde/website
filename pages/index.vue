a9314bc5a8fce2693d4fa6702a1e954ab99ee78b
Johannes Losch <hi@jlosch.de>
2022-06-25 13:19:40 +0200

    popup closed

--- pages/index.vue
+++ pages/index.vue
@@ -65,7 +65,7 @@ export default {
               tags: '',
               type: 'NULL'
             },
-            isAddItemOpen: true,
+            isAddItemOpen: false,
         };
     },
     created() {
